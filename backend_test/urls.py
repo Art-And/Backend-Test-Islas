c2e20e71e1082ff27435829c76ad908c4e73d66c
Andres Is <andres.is.rolla@gmail.com>
2022-02-13 12:01:39 -0600

    feature#1 Added basic User view, to know that url works

--- backend_test/urls.py
+++ backend_test/urls.py
@@ -1,4 +1,6 @@
-"""mysite URL Configuration
+"""Main URLs module.
+
+my site URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -13,12 +15,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from django.contrib import admin
 
 # from .utils.healthz import healthz
 
 urlpatterns = [
+    # Django Admin
     path('admin_menu', admin.site.urls),
+
+    path('', include(('menu.users.urls', 'users'), namespace='user')),
     # path("healthz", healthz, name="healthz"),
 ]
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
