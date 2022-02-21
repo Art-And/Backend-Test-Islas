3799ba3de829e9cc940a004c67b6293e089884e6
Andres Is <andres.is.rolla@gmail.com>
2022-02-21 02:33:48 -0600

    feature#3 Added form valid in signup

--- backend_test/middleware.py
+++ backend_test/middleware.py
@@ -1,6 +1,11 @@
+"""Global middlewares"""
+
+# Django
 from django.conf import settings
 from django.contrib.sessions.middleware import SessionMiddleware
 from django.utils.cache import add_never_cache_headers
+from django.shortcuts import redirect
+from django.urls import reverse
 
 
 class HealthCheckAwareSessionMiddleware(SessionMiddleware):
@@ -17,7 +22,29 @@ class HeaderNoCacheMiddleware(object):
     def __call__(self, request):
         response = self.get_response(request)
 
-        if request.method == "GET" and not response.has_header("Cache-Control"):
+        if request.method == "GET"\
+           and not response.has_header("Cache-Control"):
             add_never_cache_headers(response)
 
+        return response
+
+
+class RedirectToMenuMiddleware:
+    """Redirect to menu middleware
+
+    Every time that user is auth, the next urls list can not be load,
+    and will be redirect to user home page.
+    """
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+
+        if not request.user.is_anonymous:
+            urls = [reverse('users:login'), reverse('users:signup')]
+
+            if request.path in urls:
+                return redirect('menu:add_menu')
+
+        response = self.get_response(request)
         return response
